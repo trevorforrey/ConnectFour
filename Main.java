4f563109f0b94851e42f5a733b3a5416be2a5870
Trevor Forrey <trevor4e@gmail.com>
2015-11-08 16:14:17 -0700

    Created base for Main GUI
    I currently have placeholder images for the:
        menuTitleCover (JLabel)
        pvpButton (JButton)
        pvcButton (JButton)
    
    I'm going to create an Assets folder in the project so we can start putting in images inside

---
+++ Main.java
@@ -0,0 +1,102 @@
+import java.awt.*;       // Using AWT containers and components
+import java.awt.event.*; // Using AWT events and listener interfaces
+import javax.swing.*;    // Using Swing components and containers
+ 
+// A Swing GUI application that creates a main menu window for the user to begin a connect four game
+public class Main extends JFrame {
+ 
+   /** Constructor to setup the GUI */
+   public Main () {
+
+      // Retrieve the content-pane of the top-level container JFrame
+      // All operations done on the content-pane
+      Container container = getContentPane();
+
+      /**
+         We might want to do a flow layout so that the menu is responsive
+         Let me know what you guys think
+         -Trevor
+      **/
+      container.setLayout(new BorderLayout(0,0)); 
+
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Exit program if close-window button clicked
+      setTitle("Connect Four"); // Sets title
+      setSize(900, 700);         // Sets initial size
+      setVisible(true);          // Shows
+      setLocationRelativeTo(null); // Displays frame in the middle of the screen
+
+
+
+      // Creation of components
+
+      // Creates menu title cover and sets image for the menu
+      JLabel menuTitleCover = new JLabel();
+      menuTitleCover.setIcon(new ImageIcon("ConnectFourCover.jpg")); // Placeholder image
+
+      
+      // Creates player vs player button and sets image for the button
+      JButton pvpButton = new JButton();
+      pvpButton.setIcon(new ImageIcon("PlayerVsPlayerCover2.jpg")); // Placeholder image
+
+
+      // Creates player vs computer button and sets image for the button
+      JButton pvcButton = new JButton();
+      pvcButton.setIcon(new ImageIcon("PlayerVsComputerCover2.jpg")); // Placeholder image
+
+
+
+
+      // Components added to the frame
+
+      container.add(menuTitleCover, BorderLayout.CENTER);
+      container.add(pvpButton, BorderLayout.WEST);
+      container.add(pvcButton, BorderLayout.EAST);
+ 
+
+
+      // Action Listeners
+
+
+      // Player vs Player action listener
+      pvpButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+
+            // Components are deleted from frame, new components are brought up
+            dispose();
+            // Board gameBoard = new Board();
+
+
+            //Test
+            System.out.println("Player vs Player was clicked");
+         }
+      });
+
+
+      // Player vs Computer action listener
+      pvcButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            // Components are deleted from frame, new components are brought up
+            dispose();
+            // Board gameBoard = new Board();
+
+
+            //Test
+            System.out.println("Player vs Computer was clicked");
+         }
+      });
+   }
+ 
+
+   /** Testing Main Menu **/
+   public static void main(String[] args) {
+      // Run the GUI construction in the Event-Dispatching thread for thread-safety
+      SwingUtilities.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+            new Main(); // Constructor does the job
+         }
+      });
+   }
+}
