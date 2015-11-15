bba1cb91d9a676b2b5e8cb5899d93e4e3ae2e16b
capacityoverload <capacityoverload@gmail.com>
2015-11-15 08:14:37 +0000

    Add some of the basic logic checks for chip check

--- Board.java
+++ Board.java
@@ -11,6 +11,8 @@ public class Board implements JFrame{
   private int mTurnCount;
   private boolean mPlayerTurn;
   private boolean mIsPlaying;
+  private int mXPositionOfMouse;
+  private int mYPositionOfMouse;
 
 
   private int[] mColumnMap;
@@ -25,6 +27,8 @@ public class Board implements JFrame{
     this.mPlayerTurn = true;
     this.mColumnMap = new int[6];
     this.mChips = new Chip[6][7];
+    this.mXPositionOfMouse = 0;
+    this.mYPositionOfMouse = 0;
     
     // Fills column map with 0's
     for (int i = 0; i < 6; i++) {
@@ -48,18 +52,20 @@ public class Board implements JFrame{
 
 
     // Calls chip constructor for either player1 or player2 chip
-    private void SetChip(int playerTurn) {
-      if (playerTurn == 1) {
-        Chips[/**rowPicked**/][/**columnPicked**/] = new Player1Chip();
-      } else {
-        Chips[/**rowPicked**/][/**columnPicked**/] = new Player2Chip();
+    
+    private void SetChip(int playerTurn, int x, int y) {
+      playerTurn = playerTurn % 2;
+      if (playerTurn == 0) {
+        Chips[x][y] = new Player1Chip();
+      } else if (playerTurn == 1) {
+        Chips[x][y] = new Player2Chip();
       }
     }
 
 
     // Sets recently placed chip's visibility to true
-    private void PlaceChip(int playerTurn) {
-      
+    private void PlaceChip(int playerTurn, int x, int y) {
+      Chips[x][y].setVisible(true);
     }
 
 
@@ -77,9 +83,93 @@ public class Board implements JFrame{
       return tempCount;
     }
 
+    private boolean CheckVertical(int playerTurn, int x, int y) {
+      int tempX = x;
+      int tempY = y;
+      int tempCount = 0;
+      int player = 0;
+      
+      //Determine who's chip is being checked
+      if (Player1Chip.class.equals(Chips[y][x].getClass()) {
+        player = 1;
+      } else if (Player2Chip.class.equals(Chips[y][x].getClass()) {
+        player = 2;
+      } else {
+        player = 0;
+      }
+      
+      //if the chip is at the top
+      if (y == 0) {
+        for (int i = 1; i < 6; i++) {
+          if (Chips[i][tempX].getVisible() && Chips[i][tempX].whosChip() == player) {
+            tempCount++;
+          } else if (count > 3) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      //if the chip is at the bottom
+      else if (y == 5) {
+        for (int i = 4; i >= 0; i--) {
+          if (Chips[i][tempX].getVisible() && Chips[i][tempX].whosChip() == player) {
+            tempCount++;
+          } else if (tempCount > 3) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+      //if the chip is not at the edge of the board
+      else if (y > 0 && y < 5) {
+        boolean runCheck = true;
+        int loopCount = 1;
+        int missCount = 0;
+        while (runCheck) {
+          if (tempCount != 3) {
+            if (tempY > 0) {
+              tempY -= loopCount;
+              if (Chips[tempY][tempX].getVisible() && Chips[tempY][tempX].whosChip() == player) {
+                tempCount++;
+                loopCount++;
+              } else {
+                missCount++;
+              }
+            }
+            
+            if (tempY < 5) {
+              tempY += loopCount;
+              if (Chips[tempY][tempX].getVisible() && Chips[tempY][tempX].whosChip() == player) {
+                tempCount++;
+                loopCount++;
+              } else {
+                missCount++;
+              }
+            }
+            if (missCount == 2) {
+              runCheck = false;
+              return false;
+            }
+          } else {
+            runCheck = false;
+            return true;
+          }
+        }
+      }
+    }
+    private boolean CheckHorizontal(int x, int y) {
+      int tempX = x;
+      int tempY = y;
+    }
+    private boolean CheckDiagonal(int x, int y) {
+      int tempX = x;
+      int tempY = y;
+    }
 
-    private boolean ChipCheck(int positionX, int positionY) {
-
+    private boolean ChipCheck(int x, int y) {
+      
     }
 
     // Handles the animation of a recently placed chip
